5c49f370d4f01f25173ad72fda4b172c81baee00
jxd1337 <virgoj@protonmail.com>
2023-10-12 18:50:30 +0200

    add command execution

--- src/util/exec.go
+++ src/util/exec.go
@@ -2,9 +2,32 @@ package util
 
 import (
     "fmt"
+    "os"
+    "strings"
+    "runtime"
     "os/exec"
 )
 
-func ExecCmd(string cmd) {
-    // cmd exec here
+func ExecCmd(cmd string) {
+    // Replace $USER with the actual user env variable
+    if runtime.GOOS == "linux" {
+        userEnv := os.Getenv("USER")
+        cmd = strings.ReplaceAll(cmd, "$USER", userEnv)
+    }
+
+    var cmdInstance *exec.Cmd
+
+    if runtime.GOOS == "linux" {
+        cmdInstance = exec.Command("/bin/sh", "-c", cmd)
+    } else {
+        cmdInstance = exec.Command("cmd", "/C", cmd)
+    }
+
+    cmdInstance.Stdout = os.Stdout
+    cmdInstance.Stderr = os.Stderr
+
+    err := cmdInstance.Run()
+    if err != nil {
+        fmt.Printf("Error: %v while executing %s\n", err, cmd)
+    }
 }
